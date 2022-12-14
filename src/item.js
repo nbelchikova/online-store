d46921d3eb2e9965287144a8bd2a21d911142c4c
nbelchikova <n.belchikova@yandex.ru>
2022-12-14 17:00:41 +0600

    feat: add setting for webpack and add css and html for main page

---
+++ src/item.js
@@ -0,0 +1,15 @@
+
+export const itemsData = [
+
+      {
+        id: 1,
+        brand: '',
+        category: '',
+        description: '',
+        image1: '',
+        image2: '',
+        cost: '',
+        warehouse: '',
+      },
+
+  ];
