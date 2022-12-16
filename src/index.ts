a702359c5f5d363f35c6a1872fa9c9e41e0d224a
nbelchikova <n.belchikova@yandex.ru>
2022-12-16 14:13:49 +0600

    feat: add slider and html and css for goods page

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
 import './reset.css';
 import './index.css';
 import {itemsData} from './item';
+
+let leftArray = document.querySelector('.array-left_img') as HTMLElement | null;
+let rightArray = document.querySelector('.array-right_img') as HTMLElement | null;
+let imgSlider = document.querySelectorAll<HTMLElement>('.slider_items_img');
+let i: number=0;
+imgSlider[0].style.display= 'block';
+
+leftArray?.addEventListener('click', () => {
+  imgSlider[0].style.display= 'none';
+  imgSlider[i].style.display= 'none';
+  if (i <= 0) {
+    i= imgSlider.length-1;
+  } else {
+    i= i-1;
+  }
+  imgSlider[i].style.display= 'block';
+});
+
+
+
+rightArray?.addEventListener('click', () => {
+  imgSlider[0].style.display= 'none';
+  imgSlider[i].style.display= 'none';
+  if (i>= imgSlider.length-1) {
+    i= 0;
+  } else {
+    i= i+1;
+  }
+  imgSlider[i].style.display= 'block';
+});
+
